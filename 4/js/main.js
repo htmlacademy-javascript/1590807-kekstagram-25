92b353c6e2a1ebbde8488e8ccee782b23c026ff0
Keks <keks@htmlacademy.ru>
2022-03-04 15:49:04 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,8 @@
+import './utils.js';
+import './data.js';
+import './forms.js';
+import './photo-editing.js';
+import {createPhotoDescriptions} from './data';
+
+// eslint-disable-next-line no-console
+console.log(createPhotoDescriptions);
