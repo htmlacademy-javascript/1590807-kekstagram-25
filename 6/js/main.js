065368e175e6fa4b573a76894a3687f5145134e5
Keks <keks@htmlacademy.ru>
2022-03-16 13:17:49 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,11 @@
+import './utils.js';
+import './data.js';
+import './forms.js';
+import './thumbnails.js';
+import './forms.js';
+import {createPhotoDescriptions} from './data';
+//import {uploadPhoto} from './forms.js';
+
+//uploadPhoto();
+// eslint-disable-next-line no-console
+console.log(createPhotoDescriptions);
