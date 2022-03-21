7f40ffd52196d702f618b7f6fe556e1c6f6c0501
vassilevpm <vassilev@yandex.ru>
2022-03-21 21:04:45 +0300

    ДЗ 9/1

--- js/forms.js
+++ js/forms.js
@@ -3,7 +3,14 @@ const body = document.querySelector('body');
 const uploadFileForm = document.querySelector('#upload-file');
 const imageEditorForm = document.querySelector('.img-upload__overlay');
 const uploadCancelBtn = document.querySelector('#upload-cancel');
-//const isEscapeKey = (evt) => evt.key ==='Escape';
+
+const downScaleBtn = imageEditorForm.querySelector('.scale__control--smaller');
+const upScaleBtn = imageEditorForm.querySelector('.scale__control--bigger');
+const scaleCtrlValue = imageEditorForm.querySelector('.scale__control--value');
+const imageUploadPreview = document.querySelector('.img-upload__preview');
+
+const imgFilters = imageEditorForm.querySelectorAll('.effects__radio');
+const filtersList = imageEditorForm.querySelector('.effects__list');
 
 const openLoadForm = () => {
   imageEditorForm.classList.remove('hidden');
@@ -19,6 +26,28 @@ const openLoadForm = () => {
       imageEditorForm.classList.add('hidden');
       body.classList.remove('modal-open');
       uploadFileForm.value = '';
+    }
+  });
+  let scaleNumber = Number(scaleCtrlValue.value.slice(0, -1));
+  downScaleBtn.addEventListener('click', () => {
+    if(scaleNumber > 25) {
+      scaleNumber -= 25;
+      scaleCtrlValue.value = `${scaleNumber}%`;
+      imageUploadPreview.style.transform = `scale(${scaleNumber / 100})`;
+    }
+  });
+  upScaleBtn.addEventListener('click', () => {
+    if(scaleNumber < 100) {
+      scaleNumber += 25;
+      scaleCtrlValue.value = `${scaleNumber}%`;
+      imageUploadPreview.style.transform = `scale(${scaleNumber / 100})`;
+    }
+  });
+  filtersList.addEventListener('click', () => {
+    for (const filter of imgFilters) {
+      if(filter.checked === true) {
+        imageUploadPreview.classList.add(`effects__preview--${filter.value}`);
+      } else {imageUploadPreview.classList.remove(`effects__preview--${filter.value}`);}
     }
   });
 };
